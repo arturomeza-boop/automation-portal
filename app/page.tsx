b624acef030136c6874ec19a2f2b909562026f0f
arturomeza-boop <arturo.meza@smilepill.mx>
2026-02-24 17:20:17 -0600

    Create page.tsx
    Agrega carpeta app inicial

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Portal de Automatización</h1>
+      <p>Sistema funcionando correctamente.</p>
+    </main>
+  );
+}
