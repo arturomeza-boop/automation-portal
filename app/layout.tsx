5dacfbeedd3274a5e60d83d8593ed383813557df
arturomeza-boop <arturo.meza@smilepill.mx>
2026-02-24 17:22:53 -0600

    Create layout.tsx
    Agrega layout requerido por Next

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "Portal de Automatización",
+  description: "Sistema corporativo",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
